39869b0da0df0432747aa48608c17c86ca78db32
akicef <akicef@gmail.com>
2024-03-06 10:51:32 +0100

    Kreiran weight i length konverter - Vezbe 2 part 1

--- OTS2023_ConventorApp/Form1.Designer.cs
+++ OTS2023_ConventorApp/Form1.Designer.cs
@@ -28,13 +28,124 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rbWeight = new System.Windows.Forms.RadioButton();
+            this.rbLength = new System.Windows.Forms.RadioButton();
+            this.lbInput = new System.Windows.Forms.Label();
+            this.lbResult = new System.Windows.Forms.Label();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rbLength);
+            this.groupBox1.Controls.Add(this.rbWeight);
+            this.groupBox1.Location = new System.Drawing.Point(39, 32);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(130, 79);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Converter type";
+            // 
+            // rbWeight
+            // 
+            this.rbWeight.AutoSize = true;
+            this.rbWeight.Location = new System.Drawing.Point(29, 19);
+            this.rbWeight.Name = "rbWeight";
+            this.rbWeight.Size = new System.Drawing.Size(59, 17);
+            this.rbWeight.TabIndex = 0;
+            this.rbWeight.TabStop = true;
+            this.rbWeight.Text = "Weight";
+            this.rbWeight.UseVisualStyleBackColor = true;
+            this.rbWeight.CheckedChanged += new System.EventHandler(this.rbWeight_CheckedChanged);
+            // 
+            // rbLength
+            // 
+            this.rbLength.AutoSize = true;
+            this.rbLength.Location = new System.Drawing.Point(29, 42);
+            this.rbLength.Name = "rbLength";
+            this.rbLength.Size = new System.Drawing.Size(58, 17);
+            this.rbLength.TabIndex = 1;
+            this.rbLength.TabStop = true;
+            this.rbLength.Text = "Length";
+            this.rbLength.UseVisualStyleBackColor = true;
+            this.rbLength.CheckedChanged += new System.EventHandler(this.rbLength_CheckedChanged);
+            // 
+            // lbInput
+            // 
+            this.lbInput.AutoSize = true;
+            this.lbInput.Location = new System.Drawing.Point(28, 138);
+            this.lbInput.Name = "lbInput";
+            this.lbInput.Size = new System.Drawing.Size(35, 13);
+            this.lbInput.TabIndex = 1;
+            this.lbInput.Text = "label1";
+            // 
+            // lbResult
+            // 
+            this.lbResult.AutoSize = true;
+            this.lbResult.Location = new System.Drawing.Point(28, 164);
+            this.lbResult.Name = "lbResult";
+            this.lbResult.Size = new System.Drawing.Size(35, 13);
+            this.lbResult.TabIndex = 2;
+            this.lbResult.Text = "label2";
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(69, 135);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(100, 20);
+            this.txtInput.TabIndex = 4;
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(69, 161);
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(100, 20);
+            this.txtResult.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(94, 204);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Convert";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(215, 271);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.lbResult);
+            this.Controls.Add(this.lbInput);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton rbLength;
+        private System.Windows.Forms.RadioButton rbWeight;
+        private System.Windows.Forms.Label lbInput;
+        private System.Windows.Forms.Label lbResult;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- OTS2023_ConventorApp/Form1.cs
+++ OTS2023_ConventorApp/Form1.cs
@@ -12,9 +12,38 @@ namespace OTS2023_ConventorApp
 {
     public partial class Form1 : Form
     {
+
+        double x = 0.45359237;
         public Form1()
         {
             InitializeComponent();
+            rbWeight.Checked = true;
+            
+
+        }
+
+        private void rbWeight_CheckedChanged(object sender, EventArgs e)
+        {
+            lbInput.Text = "Pounds";
+            lbResult.Text = "KG";
+            x = 0.45359237;
+            txtInput.Text = "";
+            txtResult.Text = "";
+        }
+
+        private void rbLength_CheckedChanged(object sender, EventArgs e)
+        {
+            lbInput.Text = "Feet";
+            lbResult.Text = "Meters";
+            x = 0.3048;
+            txtInput.Text = "";
+            txtResult.Text = "";
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            txtResult.Text = (Convert.ToDouble(txtInput.Text) * x).ToString();
         }
     }
 }
